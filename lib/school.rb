15bd313dc641632e907534005269df063540bfbb
paulleerentveld <paul@veldnet.com.au>
2022-06-28 08:38:06 +1000

    lab done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name =  name
+        @roster = {}
+    end
+
+    def add_student(student,grade)
+
+        (roster[grade] ||= []) << student
+        
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        
+
+        output = {}
+        
+        @roster.each do |grade, student| 
+            output[grade] = student.sort
+        end
+        output
+
+    end
+
+end
+
+
